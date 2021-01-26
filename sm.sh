e4762fc8dc1ad51e59c9705f64b178366788326c
Adam Crow <adamcrow63@gmail.com>
2021-01-26 11:48:40 +1100

    Updated sm.sh with current auth key etc for ios

--- sm.sh
+++ sm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl https://fcm.googleapis.com/fcm/send -H "Content-Type:application/json" -X POST -d '{"notification": {"body": "this is a new body","title": "this is a new title"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "id": "1", "status": "done"}, "to": "foyvqHPrUkLblInwvQzOz3:APA91bHivnoExWHwD5bZ5-BYpTrBm3xi04T8_n7N1jPNwt6uK1qxEbCCGMNo9cIDQcdD5lpo4BKrNnaKYssLUFHcqwSCCjrKm-HVEiduhnRUdjZgeLCg7sUgSna6xCN1N9nmQKH3v8c0"}' -H "Authorization: key=AAAA_HTuyKw:APA91bEdF4e98KTB8D9eIvhrEj5RY6_v0gpO7glIzcBWazZgh567Bl-AZde6pSqEMQdXVc8eXFylfJPP-pOJbSuMeaDRSWF01EZSDf0dAqNgQhVKHC35ErdFIIwk7oc9gbmW2Js5a8MM"
+curl https://fcm.googleapis.com/fcm/send -H "Content-Type:application/json" -X POST -d '{"notification": {"body": "this is a new body","title": "this is a new title"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "id": "1", "status": "done"}, "to": "fI9g7wws40JRqhvlafKc9Y:APA91bFtee2Y3CtCU-BNbJb9gikeCqjbKRnPDy60HIH42gInk6Z-2Xy2ifEY0XwFxsZfu6qBEJ1boOOQZUR2KvBquzW-0UBF7nPPnw20qD5OK3hnIcBV_pLCbbZzocpEb663R6iSTsUc"}' -H "Authorization: key=AAAASUwTHs4:APA91bFrZgP6PydHwSpFAMp9NRDOqi9U_WDC_bYqOHARPoe0rjV3smoXADLtNDk9P4b7-16E4dM8wFaZeAECNbxl3JWmmkFiCvW9w3MGJBrAPr3dB_h13OgaI7xbISqJQD0nqyCf6W66"
 
 
 
